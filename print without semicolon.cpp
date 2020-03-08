6d096dce85cbea410a6e65d11f2acf5e31d84ee3
khageshwor <khageshwarjoshi03@gmail.com>
2020-03-08 11:11:33 -0700

    logical question added

---
+++ print without semicolon.cpp
@@ -0,0 +1,14 @@
+//program to print hello world without using semicolon
+
+
+#include<stdio.h> 
+#include<conio.h>
+
+ 
+ int main(void)    
+{    
+ if(printf("hello world")){}   
+ 
+ getch(); 
+return 0;  
+}  
