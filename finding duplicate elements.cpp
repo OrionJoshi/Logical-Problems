db51484b28aa3e81313c2e791dc8995c31e3f82f
khageshwor <khageshwarjoshi03@gmail.com>
2020-03-16 09:56:51 -0700

    logical question added

---
+++ finding duplicate elements.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>  
+#include<conio.h>
+   
+int main(void)  
+{  
+    //Initialize array   
+    int arr[] = {1, 2, 3, 4, 2, 7, 8, 8, 3};   
+      
+    //Calculate length of array arr  
+    int length = sizeof(arr)/sizeof(arr[0]);  
+      
+    printf("Duplicate elements in given array: \n");  
+    //Searches for duplicate element  
+    for(int i = 0; i < length; i++) {  
+        for(int j = i + 1; j < length; j++) {  
+            if(arr[i] == arr[j])  
+                printf("%d\n", arr[j]);  
+        }  
+    }  
+    getch();
+    return 0;  
+} 
