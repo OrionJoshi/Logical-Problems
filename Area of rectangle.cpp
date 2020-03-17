8f50893cac46582d10bbb421a93733aa5879d344
khageshwor <khageshwarjoshi03@gmail.com>
2020-03-17 05:32:18 -0700

    logical question added

---
+++ Area of rectangle.cpp
@@ -0,0 +1,23 @@
+//Program to calculate the area of the rectangle
+//Code
+#include<stdio.h>  
+#include<conio.h>
+    
+int main(void)  
+    {  
+        int width=5;  
+        int height=10;  
+        int area=width*height;  
+        printf("Area of the rectangle=%d",area);  
+        
+        getch();
+        return 0;
+    }  
+    /*
+	
+	Complexity
+
+	O(1)
+	
+	
+	*/
